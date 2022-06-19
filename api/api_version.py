7e282a04e101ec1f6ec8abbf88041bab776d9399
Jack Whelan <jackw97@live.ie>
2022-06-19 23:18:52 +0100

    Added liability operator and routes.
    Still a good few bugs to fix and error catching needed in place for both
    liabilities and persons.

--- api/api_version.py
+++ api/api_version.py
@@ -1,18 +1,18 @@
 from flask import Blueprint
 
-from api.account import account_routes
 from api.person import person_routes
+from api.liability import liability_routes
 
 api_version = Blueprint('api_version', __name__)
-api_version.register_blueprint(account_routes, name='account_routes', url_prefix='/v1')
 api_version.register_blueprint(person_routes, name='person_routes', url_prefix='/v1')
+api_version.register_blueprint(liability_routes, name='liability_routes', url_prefix='/v1')
 
 @api_version.route('/v1')
 def version_one():
     return {
         'API v1 Default Route':
             {
-                'account_routes': '/account',
-                'person_routes': '/person'
+                'person_routes': '/person',
+                'liability_routes': '/liability'
             }
         }
